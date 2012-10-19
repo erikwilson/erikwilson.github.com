4ed9f24fd3ac3f6fe5d018fcc94eca09d126f653
Erik Wilson <Erik.E.Wilson@gmail.com>
2012-10-19 14:24:26 -0700

    Replace master branch with page content via GitHub

---
+++ index.js
@@ -0,0 +1,48 @@
+
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var Mustache = require('mustache');
+var gravatar = require('gravatar');
+var moment = require('moment');
+
+function render (resume) {
+
+  var template = fs.readFileSync(path.resolve(__dirname, 'index.mustache'), 'utf8');
+
+  if (resume.basics && resume.basics.email && resume.basics.email) {
+    resume.basics.gravatar = gravatar.url(resume.basics.email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
+  }
+  resume.basics.profiles = (resume.basics.profiles || []).map(function (profile) {
+    profile.networkIcon = profile.network.toLowerCase();
+    return profile;
+  });
+  [{
+    keys: ['startDate', 'endDate'],
+    sections: [resume.work, resume.education]
+  }, {
+    keys: ['date'],
+    sections: [resume.awards]
+  }].forEach(function (opts) {
+    opts.sections
+      .filter(Boolean)
+      .forEach(function (section) {
+        section.forEach(function (event) {
+          opts.keys.forEach(function (key) {
+            if (event[key]) {
+              event[key + 'Readable'] = moment(event[key]).format('MMMM YYYY');
+            } else {
+              console.log("no " + key + " in " + JSON.stringify(event));
+            }
+          });
+        });
+      });
+  });
+
+  return Mustache.render(template, {resume: resume});
+}
+module.exports = { render: render };
